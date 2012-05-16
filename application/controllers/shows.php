bbadc615103a0fa077ba20a3c8c0401d99564245
John Grubb <therealjohnnygrubb@gmail.com>
2012-05-16 08:36:26 -0400

    shows controller working now

--- application/controllers/shows.php
+++ application/controllers/shows.php
@@ -1,26 +1,23 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class shows extends CI_Controller {
+class Shows extends CI_Controller {
   
   public function __construct() {
-    parent::__contruct();
+    parent::__construct();
     $this->load->model('shows_model');
   }
   
-  public function index() {
+  public function index() {   
+    $data['title'] = 'Shows'; 
     $data['shows'] = $this->shows_model->get_shows();
     
     $this->load->view('templates/header', $data);
     echo "hello";
-    $this->load->view('templates/footer', $data);
+    $this->load->view('templates/footer');
   }
   
   public function view($date) {
     $data['show'] = $this->shows_model->get_shows($date);
-  }
-  
-  public function new() {
-    
   }
   
 }
--- application/models/shows_model.php
+++ application/models/shows_model.php
@@ -1,4 +1,4 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Shows_model extends CI_Model {
   
